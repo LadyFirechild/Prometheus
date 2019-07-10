0837217d93de91d00954914910d33a8589dc6b61
Dennis <dmaier2406@gmail.com>
2019-07-10 12:41:03 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ Prometheus Spieldaten/Assets/Scripts/Fire_Turn_On.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire_Turn_On : MonoBehaviour
+{
+    public ParticleSystem particles;
+
+    private void Start()
+    {
+        particles = GetComponent<ParticleSystem>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            particles.gameObject.SetActive(true);
+            GlobalEvent.ActivatedFire?.Invoke(this.gameObject);
+        }
+    }
+}
