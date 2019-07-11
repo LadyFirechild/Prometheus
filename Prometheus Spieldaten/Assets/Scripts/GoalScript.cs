65f0b03922d5adc0d681d653c8ae5b548c84429c
Dennis <dmaier2406@gmail.com>
2019-07-12 00:39:54 +0200

    geholfen :)

--- Prometheus Spieldaten/Assets/Scripts/GoalScript.cs
+++ Prometheus Spieldaten/Assets/Scripts/GoalScript.cs
@@ -2,18 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GoalScript : MonoBehaviour
 {
-
     public Fire_Counter FireCounter;
+    public GameObject Panel;
+
 
     void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.F))
+        {
+            Panel.SetActive(false);
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D trigger)
     {
         if(FireCounter.collectible == FireCounter.activeFires)
         {
             SceneManager.LoadScene("");
         }
+        if(FireCounter.collectible != FireCounter.activeFires)
+        {
+            Panel.SetActive(true);
+        }
     }
-
 }
