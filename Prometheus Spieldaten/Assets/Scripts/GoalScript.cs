ee67cf983cdc6de3f3085a07656857978434b3e4
Dennis <dmaier2406@gmail.com>
2019-07-11 15:53:03 +0200

    Bewegung geht wieder

---
+++ Prometheus Spieldaten/Assets/Scripts/GoalScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoalScript : MonoBehaviour
+{
+
+    public Fire_Counter FireCounter;
+
+    void Update()
+    {
+        if(FireCounter.collectible == FireCounter.activeFires)
+        {
+            SceneManager.LoadScene("");
+        }
+    }
+
+}
