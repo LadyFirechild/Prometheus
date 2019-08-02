971eb0f42caa23cd2ffb69f15273c78abb4d68ff
Dennis <dmaier2406@gmail.com>
2019-08-02 03:12:18 +0200

    Settings erweitert
    - fehlen von Kontrast + Helligkeit

--- Prometheus Spieldaten/Assets/Scripts/Settings_Test.cs
+++ Prometheus Spieldaten/Assets/Scripts/Settings_Test.cs
@@ -2,14 +2,55 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class Settings_Test : MonoBehaviour
 {
     [SerializeField] AudioMixer audioMixer;
+    [SerializeField] Dropdown dropdownResolution;
+    Resolution[] resolution;
+
+    public void Start()
+    {
+        resolution = Screen.resolutions;
+        dropdownResolution.ClearOptions();
+        List<string> options = new List<string>();
+        int currentResolutionIndex = 0;
+        for (int i=0; i < resolution.Length; i++)
+        {
+            string option = resolution[i].width + " x " + resolution[i].height;
+            options.Add(option);
+
+            if (resolution[i].width == Screen.currentResolution.width && resolution[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        dropdownResolution.AddOptions(options);
+        dropdownResolution.value = currentResolutionIndex;
+        dropdownResolution.RefreshShownValue();
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolutions = resolution[resolutionIndex];
+        Screen.SetResolution(resolutions.width, resolutions.height, Screen.fullScreen);
+    }
 
     public void SetVolume(float volume)
     {
         audioMixer.SetFloat("volumeMaster", volume);
+    }
+
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void Fullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
     }
 
 }
