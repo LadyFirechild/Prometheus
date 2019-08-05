cbb636ce007e74124bfd2ebec173bee47b70e5c3
Vicky <v_hohmuth@web.de>
2019-08-05 17:36:22 +0200

    MainMenu
    - Optionsmenü implementiert

---
+++ Prometheus Spieldaten/Assets/Options.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Options : MonoBehaviour
+{
+    public GameObject OptionsUI;
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (OptionsUI != null)
+            {
+                bool isActice = OptionsUI.activeSelf;
+
+                OptionsUI.SetActive(!isActice);
+
+            }
+        }
+    }
+
+
+
+
+    public void OpenPanel()
+    {
+        if (OptionsUI != null)
+        {
+            bool isActice = OptionsUI.activeSelf;
+
+            OptionsUI.SetActive(!isActice);
+
+        }
+    }
+}
