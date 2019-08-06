4e2187a0357748815dffebaef4f4e7baad37439e
Dennis <dmaier2406@gmail.com>
2019-08-06 14:00:42 +0200

    Optionsmenü gefixt / schalter mit grauer tür geht jetzt

--- Prometheus Spieldaten/Assets/Scripts/Lever_Reaction.cs
+++ Prometheus Spieldaten/Assets/Scripts/Lever_Reaction.cs
@@ -5,26 +5,68 @@ using UnityEngine;
 public class Lever_Reaction : MonoBehaviour
 {
     public GameObject Door;
+    public BoxCollider2D BC;
 
-    // Update is called once per frame
+    public void Start()
+    {
+        BC = GetComponent<BoxCollider2D>();
+    }
+
+    public void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Moveable")
+        {
+            BC.isTrigger = true;
+            Door.gameObject.SetActive(false);
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Moveable")
+        {
+            BC.isTrigger = true;
+            Door.gameObject.SetActive(false);
+        }
+    }
+
+    public void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Moveable")
+        {
+            BC.isTrigger = false;
+            Door.gameObject.SetActive(true);
+        }
+        if (other.gameObject.tag == "Player")
+        {
+            Door.gameObject.SetActive(true);
+        }
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject)
+        if(collision.gameObject.tag == "Player")
         {
+            BC.isTrigger = false;
             Door.gameObject.SetActive(false);
         }
     }
 
     private void OnTriggerStay2D(Collider2D collision)
     {
-        if (collision.gameObject)
+        if (collision.gameObject.tag == "Player")
         {
+            BC.isTrigger = false;
             Door.gameObject.SetActive(false);
         }
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if(collision.gameObject)
+        if(collision.gameObject.tag == "Player")
+        {
+            BC.isTrigger = true;
+            //Door.gameObject.SetActive(true);
+        }
+        if (collision.gameObject.tag == "Moveable")
         {
             Door.gameObject.SetActive(true);
         }
