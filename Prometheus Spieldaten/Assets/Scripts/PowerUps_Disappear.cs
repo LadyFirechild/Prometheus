3657ed8abb83e7e40876733d74e7ab79c87d6746
Dennis <dmaier2406@gmail.com>
2019-08-06 15:14:06 +0200

    PowerUps verschwinden on Pick-Up

---
+++ Prometheus Spieldaten/Assets/Scripts/PowerUps_Disappear.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Prometheus
+{
+
+    public class PowerUps_Disappear : MonoBehaviour
+    {
+        public PowerUp_Size pUSize;
+
+        public GameObject[] bigPU;
+        public GameObject[] normalPU;
+        public GameObject[] smallPU;
+
+        void Start()
+        {
+            bigPU = GameObject.FindGameObjectsWithTag("BigPU");
+            smallPU = GameObject.FindGameObjectsWithTag("SmallPU");
+            normalPU = GameObject.FindGameObjectsWithTag("NormalPU");
+
+        }
+
+        void Update()
+        {
+            Debug.Log(pUSize.small);
+            Debug.Log(pUSize.normal);
+            Debug.Log(pUSize.big);
+
+            if (pUSize.big)
+            {
+                foreach (GameObject bigPowerUp in bigPU)
+                {
+                    bigPowerUp.SetActive(false);
+                }
+                foreach (GameObject normalPowerUp in normalPU)
+                {
+                    normalPowerUp.SetActive(true);
+                }
+                foreach (GameObject smallPowerUp in smallPU)
+                {
+                    smallPowerUp.SetActive(true);
+                }
+            }
+            if (pUSize.normal)
+            {
+                foreach (GameObject bigPowerUp in bigPU)
+                {
+                    bigPowerUp.SetActive(true);
+                }
+                foreach (GameObject normalPowerUp in normalPU)
+                {
+                    normalPowerUp.SetActive(false);
+                }
+                foreach (GameObject smallPowerUp in smallPU)
+                {
+                    smallPowerUp.SetActive(true);
+                }
+            }
+            if (pUSize.small)
+            {
+                foreach (GameObject bigPowerUp in bigPU)
+                {
+                    bigPowerUp.SetActive(true);
+                }
+                foreach (GameObject normalPowerUp in normalPU)
+                {
+                    normalPowerUp.SetActive(true);
+                }
+                foreach (GameObject smallPowerUp in smallPU)
+                {
+                    smallPowerUp.SetActive(false);
+                }
+            }
+        }
+    }
+}
