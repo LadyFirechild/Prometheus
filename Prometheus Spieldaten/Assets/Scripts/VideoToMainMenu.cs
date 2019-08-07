b51a2c683abb475b61a70888a847060dbd777e71
Dennis <dmaier2406@gmail.com>
2019-08-07 18:33:11 +0200

    TrailerScripte fertig

---
+++ Prometheus Spieldaten/Assets/Scripts/VideoToMainMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class VideoToMainMenu : MonoBehaviour
+{
+    public VideoPlayer vp;
+
+    void Awake()
+    {
+        vp = GetComponent<VideoPlayer>();
+    }
+
+    void Start()
+    {
+
+        vp.loopPointReached += EndReached;
+    }
+
+    void EndReached(VideoPlayer vp)
+    {
+        vp.playbackSpeed = vp.playbackSpeed / 10.0F;
+        SceneManager.LoadScene("Main_Menu");
+    }
+}
---
+++ Prometheus Spieldaten/Assets/Scripts/VideoToTutorial.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class VideoToTutorial : MonoBehaviour
+{
+
+    public VideoPlayer vp; 
+
+    void Awake()
+    {
+        vp = GetComponent<VideoPlayer>();
+    }
+
+    void Start()
+    {
+
+        vp.loopPointReached += EndReached;
+    }
+
+    void EndReached(VideoPlayer vp)
+    {
+        vp.playbackSpeed = vp.playbackSpeed / 10.0F;
+        SceneManager.LoadScene("Tutorial");
+    }
+}
