50ed678c643db2b132130ac38f740504648d5702
Vicky <v_hohmuth@web.de>
2019-08-08 03:54:18 +0200

    viel Stuff um fast nichts

---
+++ Prometheus Spieldaten/Assets/DoorShift.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorShift : MonoBehaviour
+{
+    Vector3 awakePosition;
+    public Vector3 offSSet;
+
+    public Vector3 newScaling = new Vector3(1, 1, 1);
+     Vector3 oldScaling;
+
+    Coroutine one;
+    public float duration = 0.5f;
+    
+
+    public void Start()
+    {
+        awakePosition = transform.position;
+        oldScaling = transform.localScale;
+    }
+    
+    public void StartMovementDown(bool down)
+    {
+        if (one != null)
+            StopCoroutine(one);
+
+        StartCoroutine(Moving((down) ? awakePosition + offSSet : awakePosition));
+    }
+
+    IEnumerator Moving(Vector3 ziel)
+    {
+        float elapsed = 0;
+
+        Vector3 start = transform.position;
+        Vector3 difference = ziel - start;
+
+        float remaining =difference.magnitude / (offSSet.magnitude);
+
+        float smooth; 
+
+        while (elapsed <= remaining)
+        {
+            elapsed += Time.deltaTime;
+            smooth = Mathf.SmoothStep(0, 1, elapsed / remaining);
+
+            transform.position = start + smooth * difference;
+
+            transform.localScale = Vector3.Lerp(oldScaling,newScaling, (transform.position - awakePosition).magnitude / offSSet.magnitude); // UGLY!!!
+            yield return null;
+        }
+        transform.position = ziel;
+    }
+}
--- Prometheus Spieldaten/Assets/Scripts/Totmann.cs
+++ Prometheus Spieldaten/Assets/Scripts/Totmann.cs
@@ -6,22 +6,32 @@ public class Totmann : MonoBehaviour
 {
     public GameObject Tür;
     public float timesPlayed;
- 
+
     public AudioSource switchSource;
     public AudioClip switchClip;
+
+    Vector3 startPosition;
+    public Vector3 offSSet;
+
+    Coroutine one;
+    public float duration = 0.5f;
+
+    public bool closeagain = false;
 
     public void Start()
     {
         switchSource.clip = switchClip;
-
+        startPosition = transform.position;
     }
 
     // Update is called once per frame
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject)
+        if (collision.gameObject)
         {
-            Tür.SetActive(false);
+            StartMovementDown(true);
+            //Tür.SetActive(false);
+            Tür.GetComponent<DoorShift>()?.StartMovementDown(true);
 
             if (!switchSource.isPlaying && timesPlayed == 0)
             {
@@ -31,13 +41,48 @@ public class Totmann : MonoBehaviour
         }
     }
 
-    private void OnTriggerStay2D(Collider2D collision)
+    void StartMovementDown(bool down)
     {
-        if (collision.gameObject)
-        {
-            Tür.SetActive(false);
-            //anim.SetActive(true);
-        }
+        if (one != null)
+            StopCoroutine(one);
+
+        StartCoroutine(Moving((down) ? startPosition + offSSet : startPosition));
     }
+
+    IEnumerator Moving(Vector3 ziel)
+    {
+        float elapsed = 0;
+
+        Vector3 start = transform.position;
+        Vector3 difference = ziel - start;
+
+        float remaining = Mathf.Abs(difference.magnitude / (offSSet.magnitude));
+
+        while (elapsed <= remaining)
+        {
+            elapsed += Time.deltaTime;
+            transform.position = start + Mathf.SmoothStep(0, 1, elapsed / remaining) * difference;
+            yield return null;
+        }
+        transform.position = ziel;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        StartMovementDown(false);
+        if (closeagain)
+            Tür.GetComponent<DoorShift>()?.StartMovementDown(false);
+
+        //   Tür.SetActive(true);
+    }
+
+    //private void OnTriggerStay2D(Collider2D collision)
+    //{
+    //    if (collision.gameObject)
+    //    {
+    //        Tür.SetActive(false);
+    //        //anim.SetActive(true);
+    //    }
+    //}
 
 }
