f5d67bbec02234a4cf2bc31b482891cb5c4057a8
Vicky <v_hohmuth@web.de>
2019-08-08 08:07:17 +0200

    Levelbau 3 Polishing

---
+++ Prometheus Spieldaten/Assets/GoalLvl3.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GoalLvl3 : MonoBehaviour
+{
+    public Fire_Counter FireCounter;
+    public GameObject Panel;
+
+    public GameObject Ziel_versperrt;
+    public GameObject Ziel_geöffnet;
+
+    public GameObject FeuerOn;
+    public GameObject FeuerOff;
+
+    void Start()
+    {
+        Panel.SetActive(true);
+        FeuerOn.SetActive(false);
+    }
+
+
+    void Update()
+    {
+
+        if (FireCounter.collectible == FireCounter.activeFires)
+        {
+            Destroy(Ziel_versperrt);
+            FeuerOn.SetActive(true);
+            FeuerOff.SetActive(false);
+        }
+
+
+    }
+
+    void OnTriggerStay2D(Collider2D trigger)
+    {
+        if (FireCounter.collectible == FireCounter.activeFires)
+        {
+            SceneManager.LoadScene("Tutorial");
+        }
+
+        if (FireCounter.collectible != FireCounter.activeFires)
+        {
+
+            Panel.SetActive(true);
+
+        }
+
+      
+
+    }
+}
